09a8c90156aeb1890b5c702081da3b3ee15e3e92
jiaruihua <jiaruihua@ksjgs.com>
2024-08-29 16:33:38 +0800

    opt

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "277e757735482b1de6421bc5f942e9c0",
+const RESOURCES = {"flutter_bootstrap.js": "f3a38b03fcf4533aecb0dc783ed6c645",
 "version.json": "5fc10240b73452c6a64964c44206397c",
 "index.html": "7b7b6c7bc39be1eeb92dfbd376bdf0bc",
 "/": "7b7b6c7bc39be1eeb92dfbd376bdf0bc",
-"main.dart.js": "a3780c1b76b5967be49ffa31828aff7f",
+"main.dart.js": "c63f579d1fd17f128c8a0c7a4a536076",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
